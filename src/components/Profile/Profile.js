9ba81825e7b1109b4894e86b34de0893f3f1c959
entropy88 <entropystarrover@gmail.com>
2021-11-30 22:37:08 +0200

    started implementing profile page

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,7 +1,27 @@
+import { useState, useEffect } from 'react';
+
+import { useContext } from "react/cjs/react.development";
+import {AuthContext} from "../../Contexts/AuthContext";
+import {getUser} from "../../services/auth"
+
 function Profile() {
+  const [userDetails, setUserDetails] = useState({});
+
+  const {user} =useContext(AuthContext);
+
+  useEffect(() => {
+    getUser(user.username)
+            .then(userResult => {
+                setUserDetails(userResult);
+            })
+  },[]);
+ 
+
     return (
       <>
-       <h1>This is the profile page</h1>
+       <h1>{userDetails.username}</h1>
+       <h1>{userDetails.email}</h1>
+       <h1>{userDetails.registrationDate}</h1>
       </>
     );
   }

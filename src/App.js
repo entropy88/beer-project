9f9281ac0adadde11c125e67ddd680ea33eb5180
entropy88 <entropystarrover@gmail.com>
2021-12-12 13:39:38 +0200

    implemented lazy loading with suspense on Profile

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import logo from './logo.svg';
+import React from 'react';
 import { useState, useEffect } from 'react';
 import { Routes, Route, Navigate, Outlet, useLocation } from 'react-router-dom';
 import { AuthContext } from './Contexts/AuthContext';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Home from './components/Home/Home';
-import Profile from './components/Profile/Profile';
+// import Profile from './components/Profile/Profile';
 import CreateRecord from './components/CreateRecord/CreateRecord';
 import Login from './components/Login/Login';
 import Register from './components/Register/Register';
 import BeerDetails from './components/BeerDetails/BeerDetails';
 import UpdateRecord from './components/UpdateRecord/UpdateRecord';
+
+const LazyProfile=React.lazy(()=> import('./components/Profile/Profile'))
 
 function App() {
   const [user, setUser] = useState({});
@@ -51,14 +54,24 @@ function App() {
       <Navigation/>
 
         <Routes>
+         
             <Route path="/*" element={<Home/>} />
             <Route path="/login" element={<Login/>} />            
             <Route path="/register" element={<Register/>} />
-            <Route element={<RequireAuth />}>
-            <Route path="/profile" element={<Profile />} />
+
+          
+            <Route element={<RequireAuth />}>       
+             
+            <Route path="/profile" element={
+              <React.Suspense fallback='LOADING YOUR STUFF'>
+                <LazyProfile />
+              </React.Suspense>
+            } />
+          
             <Route path="/create" element={<CreateRecord/>} />
-            <Route path="/update/:beerId" element={<UpdateRecord/>} />
-          </Route>         
+            <Route path="/update/:beerId" element={<UpdateRecord/>} />            
+            </Route>     
+           
            
           <Route path="/beers/:beerId" element={<BeerDetails/>} />
           </Routes>
